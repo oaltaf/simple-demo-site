02f31c335d530b6ccbadf333611a2367a25aa43e
osama-mavric <osama@mavrictech.com>
2024-05-25 12:10:15 +0500

    changes in test site

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-function test(){
-    alert('testing');
-  }
+function test() {
+  alert("testing");
+}
+
+function changeRoute() {
+  window.location.href = "www.google.com";
+}
+
+function goToHome() {
+  window.location.href = "https://main--demo-qasite.netlify.app/test.html";
+}
+
+function goToTestElements() {
+  window.location.href =
+    "https://main--demo-qasite.netlify.app/checkboxes.html";
+}
+
+function goToTestTable() {
+  window.location.href = "https://main--demo-qasite.netlify.app/test.html";
+}
