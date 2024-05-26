866284f7cdb510fd6c13762b5cc2c5fce0ed6ccd
osama-mavric <osama@mavrictech.com>
2024-05-26 09:28:19 +0500

    test js file

---
+++ file.js
@@ -0,0 +1,26 @@
+// // console.log("1");
+// // console.log("2");
+// // console.log("3");
+// // console.log("4");
+// // console.log("5");
+// // console.log("6");
+// // console.log("7");
+// // console.log("8");
+// // console.log("9");
+// // console.log("10");
+
+arr = [1, 2, 3, 4, 5, 6, 7, 99, 380, 333];
+// console.log(arr.length);
+console.log(arr[arr.length - 1]);
+
+// for (let i = 0; i < arr.length; i++) {
+//   //   console.log(arr.length);
+//   //   console.log(arr[i]);
+//   //0
+//   //1
+//   //2
+//   //3
+// }
+
+// console.log(a);
+// let a = 5;
